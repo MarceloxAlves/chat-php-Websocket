c0cb20ee062fcff3e0c70db7113b2910bd3d2604
Marcelo Alves <marceloalvessoft@gmail.com>
2018-11-07 11:07:59 -0300

    bacana

--- Chat.class.php
+++ Chat.class.php
@@ -19,14 +19,14 @@ class Chat implements MessageComponentInterface {
 
     public function onMessage(ConnectionInterface $from, $msg) {
         $numRecv = count($this->clients) - 1;
-        echo sprintf('IP : %s Connection %d sending message "%s" to %d other connection%s' . "\n",
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n",
             $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
 
         foreach ($this->clients as $client) {
             if ($from !== $client) {
                 // The sender is not the receiver, send to each client connected
                 $client->send(json_encode(
-                    ["msg"=> $msg, "conn"=>$from]
+                    ["msg"=> $msg, "conn"=> (array) $from]
                 ));
             }
         }
